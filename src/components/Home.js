8824b89cc399d78d6707b49d17912a138b65e06f
gauravmehta13 <270mehta@gmail.com>
2021-02-18 15:55:38 +0530

    Minor chnges in Dashboard

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,7 +29,7 @@ import { API } from "aws-amplify";
 import "./Home.css";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-
+import { makeStyles } from "@material-ui/core/styles";
 import {
   LineChart,
   Line,
@@ -55,12 +55,33 @@ import { PureComponent } from "react";
 import { Sector, Cell } from "recharts";
 import { PieChart, Pie } from "recharts";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  card: {
+    borderRadius: 0,
+    backgroundColor: "lightgrey",
+
+    boxShadow: "none",
+  },
+}));
+
 const data = [
-  { name: "Free Trucks", Trucks: 24 },
+  { name: "Free", Trucks: 24 },
   { name: "Alloted ", Trucks: 52 },
   { name: "In Transit", Trucks: 16 },
 ];
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+const delayData = [
+  { name: "Delayed Trucks", Quantity: 12 },
+  { name: "Delayed Orders ", Quantity: 52 },
+];
+const damagedData = [
+  { name: "Damaged", products: 14 },
+  { name: "Total", products: 52 },
+];
+const COLORS = ["#FF8042", "#0088FE", "#00C49F", "#FFBB28"];
 
 const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = ({
@@ -70,7 +91,7 @@ const renderCustomizedLabel = ({
   innerRadius,
   outerRadius,
   percent,
-  index,
+  name,
 }) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
@@ -80,11 +101,12 @@ const renderCustomizedLabel = ({
     <text
       x={x}
       y={y}
-      fill="white"
+      fill="black"
       textAnchor={x > cx ? "start" : "end"}
       dominantBaseline="central"
     >
-      {`${(percent * 100).toFixed(0)}%`}
+      {/* {`${(percent * 100).toFixed(0)}%`} */}
+      {name}
     </text>
   );
 };
@@ -216,7 +238,7 @@ class Home extends Component {
                     </div>
 
                     <BarChart
-                      width={500}
+                      width={550}
                       height={190}
                       data={data}
                       margin={{
@@ -250,13 +272,13 @@ class Home extends Component {
                       fontWeight: 700,
                     }}
                   >
-                    Product Types
+                    Trucks
                   </div>
                   <div>
                     <PieChart width={300} height={200}>
                       <Pie
                         isAnimationActive={false}
-                        dataKey="orders"
+                        dataKey="Trucks"
                         data={data}
                         cx={135}
                         cy={100}
@@ -292,9 +314,22 @@ class Home extends Component {
                 fontWeight: 700,
               }}
             >
-              <Card>
-                <CardContent>Total amount to be recieved</CardContent>
-                <div class="paymentText" style={{ padding: 20 }}>
+              <Card
+              // style={{
+              //   borderRadius: 0,
+              //   backgroundColor: "lightgrey",
+              //   boxShadow: "none",
+              // }}
+              >
+                <CardContent
+                  style={{
+                    padding: 10,
+                    borderBottom: `1px solid lightgrey`,
+                  }}
+                >
+                  Total amount to be recieved
+                </CardContent>
+                <div class="paymentText" style={{ padding: 40 }}>
                   Rs. 2773
                 </div>
               </Card>
@@ -311,8 +346,16 @@ class Home extends Component {
               }}
             >
               <Card>
-                <CardContent> Amount Recieved</CardContent>
-                <div class="paymentText" style={{ padding: 20 }}>
+                <CardContent
+                  style={{
+                    padding: 10,
+                    borderBottom: `1px solid lightgrey`,
+                  }}
+                >
+                  {" "}
+                  Amount Recieved
+                </CardContent>
+                <div class="paymentText" style={{ padding: 40 }}>
                   Rs. 2773
                 </div>
               </Card>
@@ -329,11 +372,231 @@ class Home extends Component {
               }}
             >
               <Card>
-                <CardContent>Amount Pending</CardContent>
-                <div class="paymentText" style={{ padding: 20 }}>
+                <CardContent
+                  style={{
+                    padding: 10,
+                    borderBottom: `1px solid lightgrey`,
+                  }}
+                >
+                  Amount Pending
+                </CardContent>
+                <div class="paymentText" style={{ padding: 40 }}>
                   Rs. 2773
                 </div>
               </Card>
+            </Grid>
+          </Grid>
+        </div>
+        <div>
+          <Grid container spacing={3} style={{ paddingTop: 20 }}>
+            <Grid
+              item
+              xs={12}
+              sm={4}
+              style={{
+                paddingTop: 10,
+                paddingBottom: 0,
+                textAlign: "center",
+                fontWeight: 700,
+              }}
+            >
+              <Card>
+                <CardContent
+                  style={{
+                    padding: 10,
+                    borderBottom: `1px solid lightgrey`,
+                  }}
+                >
+                  Total Revenue
+                </CardContent>
+                <div class="paymentText" style={{ padding: 30 }}>
+                  Rs. 2773
+                </div>
+              </Card>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={2}
+              style={{
+                paddingTop: 10,
+                paddingBottom: 0,
+                textAlign: "center",
+                fontWeight: 700,
+              }}
+            >
+              <Card>
+                <CardContent
+                  style={{
+                    padding: 10,
+                    borderBottom: `1px solid lightgrey`,
+                  }}
+                >
+                  {" "}
+                  Weekly{" "}
+                </CardContent>
+                <div class="paymentText" style={{ padding: 30 }}>
+                  Rs. 2773
+                </div>
+              </Card>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={2}
+              style={{
+                paddingTop: 10,
+                paddingBottom: 0,
+                textAlign: "center",
+                fontWeight: 700,
+              }}
+            >
+              <Card>
+                <CardContent
+                  style={{
+                    padding: 10,
+                    borderBottom: `1px solid lightgrey`,
+                  }}
+                >
+                  Monthly
+                </CardContent>
+                <div class="paymentText" style={{ padding: 30 }}>
+                  Rs. 2773
+                </div>
+              </Card>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={2}
+              style={{
+                paddingTop: 10,
+                paddingBottom: 0,
+                textAlign: "center",
+                fontWeight: 700,
+              }}
+            >
+              <Card>
+                <CardContent
+                  style={{
+                    padding: 10,
+                    borderBottom: `1px solid lightgrey`,
+                  }}
+                >
+                  Quarterly
+                </CardContent>
+                <div class="paymentText" style={{ padding: 30 }}>
+                  Rs. 2773
+                </div>
+              </Card>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={2}
+              style={{
+                paddingTop: 10,
+                paddingBottom: 0,
+                textAlign: "center",
+                fontWeight: 700,
+              }}
+            >
+              <Card>
+                <CardContent
+                  style={{
+                    padding: 10,
+                    borderBottom: `1px solid lightgrey`,
+                  }}
+                >
+                  Yearly
+                </CardContent>
+                <div class="paymentText" style={{ padding: 30 }}>
+                  Rs. 2773
+                </div>
+              </Card>
+            </Grid>
+          </Grid>
+        </div>
+        <div>
+          <Grid container spacing={3} style={{ paddingTop: 20 }}>
+            <Grid item xs={12} sm={4}>
+              <Card>
+                <CardContent>
+                  <div
+                    style={{
+                      padding: 5,
+                      paddingTop: 10,
+                      paddingBottom: 10,
+                      textAlign: "center",
+                      fontWeight: 700,
+                    }}
+                  >
+                    Total Products vs Damaged Products
+                  </div>
+                  <div>
+                    <PieChart width={300} height={200}>
+                      <Pie
+                        isAnimationActive={false}
+                        dataKey="products"
+                        data={damagedData}
+                        cx={135}
+                        cy={100}
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                      >
+                        {data.map((entry, index) => (
+                          <Cell
+                            key={`cell-${index}`}
+                            fill={COLORS[index % COLORS.length]}
+                          />
+                        ))}
+                      </Pie>
+                      <Tooltip />
+                    </PieChart>
+                  </div>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={8}>
+              <div>
+                <Card Style={{ marginBottom: 20 }}>
+                  <CardContent>
+                    <div
+                      style={{
+                        padding: 5,
+                        paddingTop: 10,
+                        paddingBottom: 20,
+                        textAlign: "center",
+                        fontWeight: 700,
+                      }}
+                    >
+                      Asset Dashboard
+                    </div>
+
+                    <BarChart
+                      width={550}
+                      height={190}
+                      data={delayData}
+                      margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                      }}
+                    >
+                      <CartesianGrid strokeDasharray="3 3" />
+                      <XAxis dataKey="name" />
+
+                      <YAxis dataKey="Quantity" />
+                      <Tooltip />
+                      <Legend />
+                      <Bar dataKey="Quantity" fill="#df4759" />
+                    </BarChart>
+                  </CardContent>
+                </Card>
+              </div>
             </Grid>
           </Grid>
         </div>
